c1fbe6b79184f1090b990784775a0459454aab6f
plumthedev <contact@kacperpruszynski.pl>
2019-04-21 19:07:12 +0200

    add travis CI and modifed readme.md

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -143,24 +143,14 @@ let webpackConfig = {
    },
    plugins: [
       new WorkboxPlugin.GenerateSW({
-         // Exclude images from the precache
-         exclude: [/\.(?:png|jpg|jpeg|svg)$/],
-   
-         // Define runtime caching rules.
+         exclude: [/\.(?:png|jpg|jpeg|svg|html|css|js)$/],
          runtimeCaching: [{
-           // Match any request ends with .png, .jpg, .jpeg or .svg.
-           urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-   
-           // Apply a cache-first strategy.
+           urlPattern: /\.(?:png|jpg|jpeg|svg|html|css|js)$/,
            handler: 'CacheFirst',
-   
            options: {
-             // Use a custom cache name.
              cacheName: 'images',
-   
-             // Only cache 10 images.
              expiration: {
-               maxEntries: 10,
+               maxEntries: 25,
              },
            },
          }],
